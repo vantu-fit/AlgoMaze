9efca924c363902f263884671be4779115d6279b
Đạt Trương <tiendatruong@gmail.com>
2024-11-05 18:34:31 +0700

    :rocket: Add fail function

--- algorithm/UCS.py
+++ algorithm/UCS.py
@@ -330,19 +330,19 @@ class UniformCostSearch:
 
 
 def main():
-    # input_filename = 'maze\\input-06.txt'
-    # output_filename = 'ucs\\output-06.txt'
-    # solver = UniformCostSearch(input_filename, output_filename)
-    # solver.solve()
-    if len(sys.argv) >= 2:
-        input_filename = sys.argv[1]
-    if len(sys.argv) >= 3:
-        output_filename = sys.argv[2]
-    for i in range(1, 10):
-        input_filename = f'map\\input-0{i}.txt'
-        output_filename = f'ucs\\output-0{i}.txt'
-        solver = UniformCostSearch(input_filename, output_filename)
-        solver.solve()
+    input_filename = 'maze\\input-01.txt'
+    output_filename = 'ucs\\output-01.txt'
+    solver = UniformCostSearch(input_filename, output_filename)
+    solver.solve()
+    # if len(sys.argv) >= 2:
+    #     input_filename = sys.argv[1]
+    # if len(sys.argv) >= 3:
+    #     output_filename = sys.argv[2]
+    # for i in range(1, 10):
+    #     input_filename = f'map\\input-0{i}.txt'
+    #     output_filename = f'ucs\\output-0{i}.txt'
+    #     solver = UniformCostSearch(input_filename, output_filename)
+    #     solver.solve()
 
 
 if __name__ == "__main__":
